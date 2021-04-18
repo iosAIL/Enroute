c7dae2a1943f85e02a86e10ffad36f15ae4a5130
iman20004 <imanali4@gmail.com>
2021-04-17 23:12:31 -0400

    Changing add package screen

--- ordertracking/SubmitInfoViewController.swift
+++ ordertracking/SubmitInfoViewController.swift
@@ -8,18 +8,25 @@
 import UIKit
 
 class SubmitInfoViewController: UIViewController {
-    @IBOutlet weak var submitButton: UIButton!
+    
     @IBOutlet weak var trackNumInput: UITextField!
     @IBOutlet weak var carrierInput: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        submitButton.layer.cornerRadius = 15
+        //submitButton.layer.cornerRadius = 15
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    
+    @IBAction func onSubmitButton(_ sender: Any) {
+        
+        
+    }
+    
+    
+    /*override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let nav = segue.destination as? UINavigationController, let trackViewController = nav.topViewController as? TrackViewController {
             trackViewController.setTrackingNumAndCarrier(trackNumInput.text, carrierInput.text)
         }
-    }
+    }*/
 }
