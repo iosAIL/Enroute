d4040d50609b0178ad6429f522db543946a10d4b
iman20004 <imanali4@gmail.com>
2021-04-17 21:33:02 -0400

    Parse setup

--- ordertracking/AppDelegate.swift
+++ ordertracking/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "Y0qIMpJDaRzlf0yHppxvXdlsROV6vaXSyAC8cxIT" // <- UPDATE
+                    $0.clientKey = "PavfihWHvAzosckQS69ntSLS4KxtIwp9DbamaOTJ" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
         return true
     }
 
